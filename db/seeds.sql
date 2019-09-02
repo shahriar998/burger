516bec8a81c33eba2d12cbba19afb4d79dd77f0b
Shahriar Elias <shahriar998@gmail.com>
2019-09-02 12:52:06 -0400

    completed connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicky Chicken Cheese Burger', true);
